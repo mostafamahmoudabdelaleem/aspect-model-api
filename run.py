b56b5798dc95263e0eea70058cc841a976a89952
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-29 16:35:33 +0200

    v3

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from gensim.models import Word2Vec
 from flask import Flask, jsonify
+import time
 
 app = Flask(__name__)
-model = None
+
 MODEL_URL = 'https://storagerat.blob.core.windows.net/telda/word2vec.model?sp=r&st=2020-07-29T14:24:29Z&se=2020-12-29T22:24:29Z&spr=https&sv=2019-12-12&sr=b&sig=KmUc2NjsbLa9NHrs4uytJ5WCxQUKymW2pbKVS%2F1GGyo%3D'
+st = time.time()
+print('Loading Model .....')
+model = Word2Vec.load(MODEL_URL)
+ed = time.time() - st
+print('Model Loaded in : {0} s'.format(ed))
 
 @app.route('/')
 def hello():
@@ -12,6 +18,7 @@ def hello():
 
 @app.route('/word2vec/<string:word>')
 def hello_user(word):
+    print('Word is: {0}'.format(word))
     data = {
         "word" : word,
         "vector": model[word].tolist()
@@ -19,5 +26,4 @@ def hello_user(word):
     return jsonify(data)
 
 if __name__ == '__main__':
-    model = Word2Vec.load(MODEL_URL)
     app.run(debug = True)
