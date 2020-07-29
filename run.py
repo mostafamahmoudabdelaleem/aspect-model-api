31d9c7f8bfd8684d94c866126f9255b422780e89
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-30 00:14:34 +0200

    v4

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from gensim.models import Word2Vec
+from gensim.models import Word2Vec, KeyedVectors
 from flask import Flask, jsonify
 import time
 import threading
 
 app = Flask(__name__)
 
-MODEL_URL = 'https://storagerat.blob.core.windows.net/telda/word2vec-100.model?sp=r&st=2020-07-29T17:11:38Z&se=2020-12-30T01:11:38Z&spr=https&sv=2019-12-12&sr=b&sig=0uLLfwejVAc%2FcDR3sESRgDIzaRSGvjCWpZrSWYZ57Xc%3D'
+MODEL_URL = 'https://storagerat.blob.core.windows.net/telda/word2vec-150-400k.bin?sp=r&st=2020-07-29T22:12:06Z&se=2020-08-30T06:12:06Z&spr=https&sv=2019-12-12&sr=b&sig=l7lQ8gSusC81xeobOP6rzTbARjvq89pSIYCj2uuvYfo%3D'
 model = None
 
 
@@ -14,7 +14,8 @@ def load_model():
     if(model == None):
         st = time.time()
         print('Loading Model .....')
-        model = Word2Vec.load(MODEL_URL)
+        #model = Word2Vec.load(MODEL_URL)
+        model = KeyedVectors.load_word2vec_format(MODEL_URL, binary=True)
         ed = time.time() - st
         print('Model Loaded in : {0} s'.format(ed))
 
