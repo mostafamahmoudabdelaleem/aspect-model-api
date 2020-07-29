d4e28d69511e02be49e506eefc402fc25ed65fce
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-29 19:26:03 +0200

    v4

--- run.py
+++ run.py
@@ -5,11 +5,14 @@ import time
 app = Flask(__name__)
 
 MODEL_URL = 'https://storagerat.blob.core.windows.net/telda/word2vec-100.model?sp=r&st=2020-07-29T17:11:38Z&se=2020-12-30T01:11:38Z&spr=https&sv=2019-12-12&sr=b&sig=0uLLfwejVAc%2FcDR3sESRgDIzaRSGvjCWpZrSWYZ57Xc%3D'
-st = time.time()
-print('Loading Model .....')
-model = Word2Vec.load(MODEL_URL)
-ed = time.time() - st
-print('Model Loaded in : {0} s'.format(ed))
+model = None
+
+if(model == None):
+    st = time.time()
+    print('Loading Model .....')
+    model = Word2Vec.load(MODEL_URL)
+    ed = time.time() - st
+    print('Model Loaded in : {0} s'.format(ed))
 
 @app.route('/')
 def hello():
